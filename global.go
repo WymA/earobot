300d4a9efdc9397e233c0dc78a483d5efe887fcd
WymA <mathews2wym@gmail.com>
2020-09-22 01:53:44 +0800

    add DistVector

--- global.go
+++ global.go
@@ -1,5 +1,7 @@
 package earobot
 
+import "math"
+
 // Minimal Fast Sort
 func MinFastSort(x []float64, idx []int, n int, m int) {
 
@@ -11,4 +13,25 @@ func MinFastSort(x []float64, idx []int, n int, m int) {
 			}
 		}
 	}
+}
+
+// DistVector calculates the distance of two vector
+func DistVector(vectors1 []float64, vectors2 []float64) float64 {
+
+	sum := 0.0
+	for index, vec1 := range vectors1 {
+		sum += (vec1 - vectors2[index]) * (vec1 - vectors2[index])
+	}
+
+	return math.Sqrt(sum)
+}
+
+// ObservePointDistVector calculates the distance of two vector
+func ObservePointDistVector(vectors1 []float64, vectors2 []float64) float64 {
+	sum := 0.0
+	for index, vec1 := range vectors1 {
+		sum += (vec1 - vectors2[index]) * (vec1 - vectors2[index])
+	}
+
+	return math.Sqrt(sum)
 }

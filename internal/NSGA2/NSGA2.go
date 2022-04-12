5bdb8848944f8926ede1b2134408f98c7314818d
WymA <mathews2wym@gmail.com>
2022-04-13 02:08:09 +0800

    reorganize NSGA2

--- internal/NSGA2/NSGA2.go
+++ internal/NSGA2/NSGA2.go
@@ -1,41 +1,63 @@
 package snga
 
-type SNGAInd struct {
+type NSGA2Ind struct {
+	Variables      []float64
+	Objectives     []float64
+	Rank           int
+	CrowdDist      float64
+	DominatedCount int
+
+	// SectorialAngle float64
+	// SectorialIndex int
+
+}
+
+func (rcvr *NSGA2Ind) RandomInit(varRange int) {
+
+}
+
+// func (rcvr *NSGA2Ind) ObjEvaluation() {
+
+// }
+
+type NSGA2Population struct {
+	Individuals []NSGA2Ind
+	ParetoFront [][]NSGA2Ind
 }
 
 //uptate the extreme point , that's f up/down
-func update_extreme_point(ind *SNGAInd) {
+func update_extreme_point(ind *NSGA2Ind) {
 
 }
 
 func init_population() {
 
 }
-func Pareto_HyperVolume_compare_sectorialgrid(ind *SNGAInd) {
+func Pareto_HyperVolume_compare_sectorialgrid(ind *NSGA2Ind) {
 
 }
-func GetFastigiateHyperVolume(ind *SNGAInd, indIndex int, ReferencePoint []float64) {
-
-}
-
-func compute_hypervolume(mysectorpop []SNGAInd, mypopsize int, mynobj int) {
-
-}
-func tour_selection_hv_difference(p int, mypopulation []SNGAInd) {
-
-}
-func tour_selection_hv2(mypopulation []SNGAInd) {
+func GetFastigiateHyperVolume(ind *NSGA2Ind, indIndex int, ReferencePoint []float64) {
 
 }
 
-func uniform_selection(ind_selected *SNGAInd) {
+func compute_hypervolume(mysectorpop []NSGA2Ind, mypopsize int, mynobj int) {
+
+}
+func tour_selection_hv_difference(p int, mypopulation []NSGA2Ind) {
+
+}
+func tour_selection_hv2(mypopulation []NSGA2Ind) {
+
+}
+
+func uniform_selection(ind_selected *NSGA2Ind) {
 
 }
 func reset_angle() {
 
 }
 
-func population2front(mypopulation []SNGAInd, population_front [][]float64) {
+func population2front(mypopulation []NSGA2Ind, population_front [][]float64) {
 
 }
 func calc_distance() {
